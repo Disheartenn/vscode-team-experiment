5fe80ce9a91d6d386c2202a3b6085bdd3c322523
mengshaohan <2765523467@qq.com>
2025-04-14 16:58:19 +0800

    Add goodbye function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # main.py
- def greet(name):
+def greet(name):
  print(f"Hello, {name}!")
  greet("GitHub")
+ # main.py
+ def greet(name):
+ print(f"Hello, {name}!")
+ def goodbye(name):
+ print(f"Goodbye, {name}!")
+ greet("GitHub")
+ goodbye("GitHub")
